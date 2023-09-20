5739945b4c22f22770a84e3f3096ec4a25b21c61
Ali Cagatay <aliccagatay@gmail.com>
2023-09-20 12:21:21 +0100

    Edited `App.css` and `App.js` by the starter code given by Frontend Mentor.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <body>
+        Your Result 76 of 100 Great You scored higher than 65% of the people who
+        have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
+        61 / 100 Visual 72 / 100 Continue
+        <div class="attribution">
+          Challenge by{" "}
+          <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+            Frontend Mentor
+          </a>
+          . Coded by <a href="#">Your Name Here</a>.
+        </div>
+      </body>
     </div>
   );
 }
