ee965f272e0c2ec735b2c77d61640f91a1d1e600
Ali Cagatay <aliccagatay@gmail.com>
2023-09-21 15:04:11 +0100

    Updated `MainComponent.js`.

---
+++ src/Components/MainComponent.js
@@ -0,0 +1,11 @@
+import "../App.css";
+
+function MainComponent() {
+  return (
+    <div className="MainComponent">
+      <h1>Hello World</h1>
+    </div>
+  );
+}
+
+export default MainComponent;
