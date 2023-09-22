afd874a9cb34960b9583ec9afd63cd6d88a10be9
Ali Cagatay <aliccagatay@gmail.com>
2023-09-22 13:44:27 +0100

    Updated `ResultComponent.js`.

--- src/Components/ResultComponent.js
+++ src/Components/ResultComponent.js
@@ -1,5 +1,18 @@
 import "../App.css";
 
-function ResultComponent() {}
+function ResultComponent() {
+  return (
+    <div className="ResultComponent">
+      <h3>Your Result</h3>
+      <div className="CircularResultsBorder">
+        <h1>76</h1>
+        <p>of 100</p>
+      </div>
+      <h1>Great</h1>
+      <p>You scored higher than 65% of the </p>
+      <p>people who have taken these tests.</p>
+    </div>
+  );
+}
 
 export default ResultComponent;
