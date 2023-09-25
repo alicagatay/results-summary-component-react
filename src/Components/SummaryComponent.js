1bc9fe54cf6003ec5f7d2e6fe28765f9e4664fb5
Ali Cagatay <aliccagatay@gmail.com>
2023-09-25 17:07:06 +0100

    Updated `SummaryComponents.js` to add 4 columns and a button to the Summary component.

--- src/Components/SummaryComponent.js
+++ src/Components/SummaryComponent.js
@@ -4,6 +4,11 @@ function SummaryComponent() {
   return (
     <div className="SummaryComponent">
       <h2>Summary</h2>
+      <div className="SummaryComponentReactionRow"></div>
+      <div className="SummaryComponentMemoryRow"></div>
+      <div className="SummaryComponentVerbalRow"></div>
+      <div className="SummaryComponentVisualRow"></div>
+      <div className="SummaryComponentContinueButton"></div>
     </div>
   );
 }
